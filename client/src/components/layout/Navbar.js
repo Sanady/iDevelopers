308758a1db548df0c08acc6f7ad3fd4000a4ebda
Ivan Rener <renerivan@outlook.com>
2020-03-20 19:26:33 +0100

    Section 8 - Finished

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../action/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+    const authLinks = (
+        <ul>
+            <li><a onClick={ logout } href="!#">Logout</a></li>
+        </ul>
+    );
+
+    const guestLinks = (
+        <ul>
+            <li><a href="!#">Developers</a></li>
+            <li><Link to='/register'>Register</Link></li>
+            <li><Link to='/login'>Login</Link></li>
+        </ul>
+    );
+
     return (
         <div>
             <nav className="navbar bg-dark">
                 <h1>
                     <Link to='/'><i className="fas fa-code"></i>iDevelopers</Link>
                 </h1>
-                <ul>
-                    <li><Link to="!#">Developers</Link></li>
-                    <li><Link to='/register'>Register</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
-                </ul>
+                {!loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
             </nav>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,66 +1,80 @@
 const express = require("express");
-const { check, validationResult } = require('express-validator');
-const config = require('config');
-const bcrypt = require('bcryptjs');
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const User = require('../../models/User');
+const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
-//@route    GET api/auth
-//@desc     Test route
-//@access   Public
-router.get('/', async (req, res) => {
-    try{
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    }catch(err){
-        console.log(err.message);
-        res.status(500).send('Server error');
-    }
+const User = require("../../models/User");
+
+// @route    GET api/auth
+// @desc     Get user by token
+// @access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
-//@route    GET api/auth
-//@desc     Authenticate user & get token
-//@access   Public
-router.post('/', [
-    check('email', 'Please include a valid email!').isEmail(),
-    check('password', 'Password is required!').exists()
-], async (req, res) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
+
     try {
-        let user = await User.findOne({ email })
-        if (user) {
-            return res.status(400).json({ errors: [{ mesg: 'Invalid credentials!' }] });
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
         }
+      };
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch){
-            return res.status(400).json({ errors: [{ mesg: 'Invalid credentials!' }] });
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
         }
-
-        const payload = {
-            user: {
-                id: user.id
-
-            }
-        };
-
-        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600 },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token });
-            });
-
-
+      );
     } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server error');
+      console.error(err.message);
+      res.status(500).send("Server error");
     }
-});
+  }
+);
 
 module.exports = router;
