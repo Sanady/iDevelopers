65206cda96dba55855576ed00c6b466481f9b027
Ivan Rener <renerivan@outlook.com>
2020-03-20 11:09:39 +0100

    Section 7 - Finished

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,8 +1,6 @@
 import { SET_ALERT, REMOVE_ALERT } from '../action/types';
 
-const initialState = [
-    
-];
+const initialState = [];
 
 export default function(state = initialState, action){
     const { type, payload } = action;
