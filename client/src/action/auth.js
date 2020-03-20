a20118eec1a8a08688c3e717daa8d825c7ffcd46
Ivan Rener <renerivan@outlook.com>
2020-03-20 19:45:22 +0100

    Section 9 - Prettier code format

--- client/src/action/auth.js
+++ client/src/action/auth.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import { setAlert } from './alert';
+import axios from "axios";
+import { setAlert } from "./alert";
 import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    USER_LOADED,
-    AUTH_ERROR,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT
-} from './types';
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
+} from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
 // Load User
@@ -17,7 +17,7 @@ export const loadUser = () => async dispatch => {
     setAuthToken(localStorage.token);
   }
   try {
-    const res = await axios.get('/api/auth');
+    const res = await axios.get("/api/auth");
     dispatch({
       type: USER_LOADED,
       payload: res.data
@@ -30,33 +30,33 @@ export const loadUser = () => async dispatch => {
 };
 
 export const register = ({ name, email, password }) => async dispatch => {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
     }
+  };
 
-    const body = JSON.stringify({ name, email, password });
+  const body = JSON.stringify({ name, email, password });
 
-    try {
-        const res = await axios.post('/api/users', body, config);
+  try {
+    const res = await axios.post("/api/users", body, config);
 
-        dispatch({
-            type: REGISTER_SUCCESS,
-            payload: res.data
-        });
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
 
-        dispatch(loadUser());
-    } catch (err) {
-        const errors = err.response.data.errors;
-        if(errors){
-            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-        }
-        dispatch({
-            type: REGISTER_FAIL
-        });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
     }
-}
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
+};
 
 export const login = (email, password) => async dispatch => {
   const config = {
@@ -90,4 +90,4 @@ export const login = (email, password) => async dispatch => {
 //Logout / clear profile:
 export const logout = () => dispatch => {
   dispatch({ type: LOGOUT });
-}
+};
